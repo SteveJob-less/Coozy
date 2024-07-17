8d802807a29e590c46f2077bc750eecabc7588e8
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-18 01:04:38 +0800

    Delete: removed the unnecessary frontend app (/frontendx) from this repo

--- frontendx/src/components/layouts/_reusables/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="lg:h-[10dvh] bg-red-300">
-            
-        </footer>
-    );
-};
-
-export default Footer;
--- frontendx/src/components/pages/UserAuthPage.tsx
+++
@@ -1,9 +0,0 @@
-import UserForm from "../section/UserAuthPage/UserForm";
-
-const UserAuthPage = () => {
-    return (
-        <UserForm />
-    );
-};
-
-export default UserAuthPage;
