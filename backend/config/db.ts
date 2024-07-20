ac3d1c802115ed6e41bef6d5da42d50a7c106de8
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-20 14:12:37 +0800

    Configured the sequelize instance to match model name and mysql table that uses snake_case

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -9,7 +9,10 @@ const db = new Sequelize(
     process.env.DB_PASSWORD ?? "",
     { 
         host: process.env.DB_HOST ?? "", 
-        dialect: "mysql" 
+        dialect: "mysql",
+        dialectOptions: {
+            underscored: true, // Use snake_case for table names
+        },
     }
 );
 
