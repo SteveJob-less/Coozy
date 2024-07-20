1f8219d8fe46e325728aad7d2ad3418a3eb5a51d
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-20 14:42:37 +0800

    Created a controller for creating new user

---
+++ backend/src/controllers/user/signupUser.ts
@@ -0,0 +1,33 @@
+import { User } from "../../models/user";
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+const signupUser = async (req: Request, res: Response) => {
+    try {
+        const { Firstname, Lastname, Email, Password } = req.body;
+
+        const existingUser = await User.findOne({ where: { email: Email } });
+
+        if (existingUser) {
+            console.log("User already exists");
+            return res.status(409).json({ success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(Password, 10);
+
+        const newUser = await User.create({
+            firstname: Firstname,
+            lastname: Lastname,
+            email: Email,
+            address: "",
+            password: hashedPassword,
+        });
+
+        res.status(201).json({ success: true, message: "User created successfully" });
+    } catch (error: any) {
+        console.error("Error during signup:", error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+export default signupUser;
