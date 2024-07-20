527e398afc25d2f771ed3ed25f24555fa22a77a2
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-20 14:36:28 +0800

    Used cors and configured it to allow my frontent's http requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express, { Router, Request, Response } from "express";
 import bodyParser from "body-parser"; 
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
