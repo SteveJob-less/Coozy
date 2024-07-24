7df800c06fe72aa86ba17e2c0e249d879dfb2d40
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-24 19:27:37 +0800

    Updated: from secure: true to secure: false since it is currently in development and app is using http requests

--- backend/config/cookieOptions.ts
+++ backend/config/cookieOptions.ts
@@ -1,6 +1,6 @@
 const cookieOptions = {
     // Don't forget to set this to true if using HTTPS
-    // secure: true, 
+    secure: false, 
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
 };
