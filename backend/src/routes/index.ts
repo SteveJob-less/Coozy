71a08bd5199ee9f05d32e10c213b4e855900b062
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-24 20:09:55 +0800

    Created new routes for: token refresh, add to cart, and to fetch user cart

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import signupUser from "../controllers/user/signupUser";
 import loginUser from "../controllers/user/loginUser";
+import addToCart from "../controllers/cart/addToCart";
+import verifyJWT from "../middleware/verifyJWT";
+import handleRefreshToken from "../controllers/handleRefreshToken";
+import getUserCart from "../controllers/cart/getUserCart";
 
 const router = Router();
 
 router.post("/user/signup", signupUser);
 router.post("/user/login", loginUser);
+router.post("/user/token-refresh", handleRefreshToken);
+router.post("/cart/add", verifyJWT, addToCart);
+router.get("/cart", verifyJWT, getUserCart);
 
 export default router;
