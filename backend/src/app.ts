63086e22e24b4ddecb7b88f87422c1f2596fc4bd
Steve A <123392509+SteveJob-less@users.noreply.github.com>
2024-07-24 19:30:25 +0800

    Enabled cookie parsing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,13 @@ import bodyParser from "body-parser";
 import routes from "./routes";
 import cors from "cors";
 import corsOptions from "../config/corsOptions";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors(corsOptions));
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
